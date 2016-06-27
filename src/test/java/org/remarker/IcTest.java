b60b6f939e9971edf58294f0823622a262e718db
Justin Sampson <jsampson@guidewire.com>
2016-06-27 13:01:26 -0700

    Added support for Intercooler attributes.

---
+++ src/test/java/org/remarker/IcTest.java
@@ -0,0 +1,43 @@
+package org.remarker;
+
+import junit.framework.TestCase;
+import org.jdom.output.XMLOutputter;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import static org.remarker.Html.*;
+
+public class IcTest extends TestCase
+{
+    public void testIcAttributeAllowedWithXmlOutputter() throws IOException
+    {
+        XMLOutputter outputter = new XMLOutputter();
+        StringWriter writer = new StringWriter();
+        outputter.output(HTML(BODY(DIV(Ic.GetFrom("foo")))), writer);
+        assertEquals("<html><body><div ic-get-from=\"foo\" /></body></html>", writer.toString());
+    }
+
+    public void testIcAttributeAllowedWithHtmlOutputterWhenEnabled() throws IOException
+    {
+        StringWriter writer = new StringWriter();
+        HtmlOutputter outputter = new HtmlOutputter(writer, ElementDefinition.DTD.STRICT).allowIcAttributes();
+        outputter.output(HTML(BODY(DIV(Ic.GetFrom("foo")))));
+        assertEquals("<HTML>\r\n  <BODY>\r\n    <DIV ic-get-from=\"foo\"></DIV>\r\n  </BODY>\r\n</HTML>\r\n", writer.toString());
+    }
+
+    public void testIcAttributeNotAllowedWithHtmlOutputterWhenNotEnabled() throws IOException
+    {
+        StringWriter writer = new StringWriter();
+        HtmlOutputter outputter = new HtmlOutputter(writer, ElementDefinition.DTD.STRICT);
+        try
+        {
+            outputter.output(HTML(BODY(DIV(Ic.Target("foo")))));
+            fail();
+        }
+        catch (IllegalArgumentException expected)
+        {
+            assertEquals("The 'ic-target' attribute is not allowed for the 'div' element with the strict DTD", expected.getMessage());
+        }
+    }
+}
